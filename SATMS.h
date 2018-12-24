ea0c094d7ed8f8ff97668c238ae78a303012fa6e
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-24 16:44:31 +0800

    Update SATMS.h

--- SATMS.h
+++ SATMS.h
@@ -1,33 +1,16 @@
 #pragma once
-#ifndef SUCCESS
-#define SUCCESS 1//标志位成功
-#endif 
-#ifndef FAILURE 
-#define FAILUER 0//标志位失败
-#endif
-#ifndef INF
-#define INF 0x3f3fffff//标志位无穷
-#endif
-#ifndef MAXNUM
-#define MAXNUM 20
-#endif
-
-#include<stdio.h>
+#include <graphics.h>
+#include <time.h>
+#include <math.h>
+#include<Windows.h>
 #include<iostream>
 using namespace std;
-typedef bool Status;//定义函数状态数据类型
-//Status Vertex[MAXNUM];
-typedef char Vertextype[MAXNUM][MAXNUM];//定义顶点向量数据类型
-typedef int AdjMatrix[MAXNUM][MAXNUM];//定义邻接矩阵数据类型
+//图片输出函数
+void putBackgroundimgsky();//输出菜单背景图片
+void putBgMap();//输出地图背景
 
-typedef struct Graph {//定义图数据类型
-	Vertextype Vexs;//图的顶点向量
-	AdjMatrix Arcs;//图的邻接矩阵
-	int VexNum;//图的当前顶点
-	int ArcNum;//图的当前弧
-}*pGraph;//定义图的指针数据类型
-
-typedef struct CloseEdge {//定义辅助数组数据类型
-	Vertextype Vexs;//图的顶点向量
-	int Lowcost[MAXNUM];//最短路径
-}*pCloseEdge;//定义辅助数组指针数据
+void putMenu();//输出主菜单
+int input();//从键盘读入一个数字字符，返回该数字int型
+int Mouse();//点击功能模块，返回其对应序号
+int inputInfoY();//输入Y坐标值，并返回该值
+int inputInfoX();//输入X坐标值，并返回该值数据
