387b64e4329d033f997283329f8f76952b4f9782
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-26 09:14:30 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,16 @@
 #include "SATMS.h"
-#include "classPutImg.h"
+#include "graph.h"
 
 int main() {
 	/*setinitmode(INIT_NOBORDER);//设置控制台无边框*/
 	initgraph(1000, 600);
-	/*Mouse();*/
-	putBackgroundimgsky();//背景图片
-	putMenu();//菜单
-	switch (Mouse()){
-		case 1: {cleardevice();
-				putBgMap();
-				putImg a(100, 100);
-				putImg b(200, 200); 
-
-				int x = inputInfoX();
-				int y = inputInfoY();
-				char Xlocate[10];
-				char Ylocate[10];
-				outtext(_itoa(x, Xlocate, 10));
-				outtext(_itoa(y, Ylocate, 10));
-				/*char str[100];
-				inputbox_getline("创建景点",
-					"请输入该景点的名称：",
-					str,
-					sizeof(str) / sizeof(*str));*/
-				
-				break; }
-				
-		case 0: {exit(0); }
-		 
-	default:
-		break;
-	}
+	setcaption("The Great 234");
+	putBackgroundimgsky();
+	putMenu();
+	MGraph G;
+	Mouse1(&G);
+	MUSIC music;
+	
 	getch();
 	closegraph();
 	return 0;
