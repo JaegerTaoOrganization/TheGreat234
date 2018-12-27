e304cae3d476d947d840d1ee979fec532196459a
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-27 08:46:57 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,13 @@
-#include "SATMS.h"
+
 #include "graph.h"
 
 int main() {
 	/*setinitmode(INIT_NOBORDER);//设置控制台无边框*/
 	initgraph(1000, 600);
 	setcaption("The Great 234");
-	putBackgroundimgsky();
-	putMenu();
+	setbkmode(TRANSPARENT);
 	MGraph G;
-	Mouse1(&G);
-	MUSIC music;
+	Mouse0(&G);
 	
 	getch();
 	closegraph();
