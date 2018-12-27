4e2de82aa74be5f209d89ccde14ec5a70b152719
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-27 08:49:01 +0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -31,13 +31,13 @@ extern void circle(MGraph G);//判断是否有回路
 extern void MiniTree_Prim(MGraph);//判断最小生成树,普里姆算法
 extern void ShortPath_Floyd(MGraph G, int P[][30], int D[][30]);//弗洛伊德算法，求最短距离
 void Mouse1(MGraph *G);//点击功能模块，返回其对应序号
-void Mouse2(MGraph *G);
-
+void Mouse2(MGraph *G,int i);
+void Mouse0(MGraph *G);
 //页面切换函数
 //创建页面
-void pageCreate() {
+void pageCreate(int i) {
 	cleardevice();
-	putBgMap();
+	putBgMap(i);
 }//创建页面
 
 
@@ -100,11 +100,11 @@ void CreateRoad(MGraph *G) {
 		w = (int)pow(pow(vex[i].x-vex[j].x,2)+pow(vex[i].y-vex[j].y,2),0.5);//求距离
 		G->arc[i][j] = w;
 		G->arc[j][i] = G->arc[i][j];
-		putline(vex[i].x + 25, vex[i].y + 50, vex[j].x + 25, vex[j].y + 50);
+		/*putline(vex[i].x + 25, vex[i].y + 50, vex[j].x + 25, vex[j].y + 50);
 		char outw[10];
-		_itoa(w,outw,10);
+		_itoa_s(w,outw,4,10);
 		setcolor(WHITE);
-		outtextxy((vex[i].x + vex[j].x) / 2, (vex[i].y + vex[j].y) / 2, outw);
+		outtextxy((vex[i].x + vex[j].x) / 2, (vex[i].y + vex[j].y) / 2, outw);*/
 	}	
 }
 void DFS(MGraph G, int i) {
@@ -129,6 +129,18 @@ void DFSTraverse(MGraph G) {
 		if (!visit[i]) {
 			DFS_count++;//判断几个连通分量
 			DFS(G, i);//未访问过的点进入递归循环，如果只有一个连通分量，只会执行一次
+		}
+	}
+	for (i = 0; i < G.numvertextype; i++) {
+		for (int j = 0; j < G.numvertextype; j++) {
+			if (G.arc[i][j] != Wuqiong) {
+				setcolor(RED);
+				putline(vex[i].x + 25, vex[i].y + 50, vex[j].x + 25, vex[j].y + 50);
+				char outw[10];
+				_itoa_s(G.arc[i][j], outw, 4, 10);
+				setcolor(WHITE);
+				outtextxy((vex[i].x + vex[j].x) / 2, (vex[i].y + vex[j].y) / 2, outw);
+			}
 		}
 	}
 }
@@ -162,7 +174,8 @@ void MiniTree_Prim(MGraph G) {
 		}
 		/*printf("(%s %d %d)", vex[adjvex[k]].Vex,  vex[adjvex[k]].x, vex[adjvex[k]].y);
 		printf("(%s %d %d)", vex[k].Vex, vex[k].x, vex[k].y);*/
-		putline(vex[adjvex[k]].x+3,vex[adjvex[k]].y+3,vex[k].x+3,vex[k].y+3);
+		setcolor(BLUE);
+		putline(vex[adjvex[k]].x+2,vex[adjvex[k]].y+2,vex[k].x+2,vex[k].y+2);
 		lowcost[k] = 0;//当前权值为0，表示已经录入这条边
 		for (j = 1; j < G.numvertextype; j++) {
 			if (lowcost[j] != 0 && G.arc[k][j] < lowcost[j]) {//因为与第一个顶点无关的都设为了无穷，这里就把与选中的第二个顶点有关的边的权值录入，原本是无穷。不等于0的原因是不录入已经录入的边
@@ -185,21 +198,67 @@ void ShortPath_Floyd(MGraph G, int P[][30], int D[][30]) {
 			for (w = 0; w < G.numvertextype; w++) {
 				if (D[v][w] > D[v][k] + D[k][w]) {
 					D[v][w] = D[v][k] + D[k][w];//暴力求解，k值为此次循环路径必经过的点，所以它会求出所有点到所有点的最短距离，无穷必被替代
+					P[v][w] = P[v][k];//经过哪些路径的下标显示
 				}
-				P[v][w] = P[v][k];//经过哪些路径的下标显示
 			}
 		}
 	}
-	printf("输入你要查找哪个点到哪个点的最短距离\n");
+	/*printf("输入你要查找哪个点到哪个点的最短距离\n");
 	scanf_s("%d %d", &v, &w);
-	printf("%d\n", D[v][w]);
+	printf("%d\n", D[v][w]);*/
+	char str[10] = "0";
+	inputbox_getline("查找两个景点最短路径",
+		"请输入起点序号：",
+		str,
+		sizeof(str) / sizeof(*str));
+	v = atoi(str);
+	inputbox_getline("查找两个景点最短路径",
+		"请输入终点序号：",
+		str,
+		sizeof(str) / sizeof(*str));
+	w = atoi(str);
+	//printf("%s %d %d\n", vex[v].Vex, vex[v].x, vex[v].y);//源点名字，x坐标，y坐标
+	putImg(vex[v].x, vex[v].y);
+	setfont(25, 10, "华文琥珀");
+	outtextxy(vex[v].x - 5, vex[v].y + 45, vex[v].Vex);
+	k = P[v][w];
+	int xx = vex[v].x; 
+	int yy = vex[v].y;
+	putImg(xx, yy);
+	while (k != w) {
+		//printf("%s %d %d\n", vex[k].Vex, vex[k].x, vex[k].y);//经过的中途各个顶点的名字，x坐标，y坐标
+		putImg(vex[v].x, vex[v].y);
+		setfont(25, 10, "华文琥珀");
+		for(;is_run()&&xx+60<vex[v].x&&yy<vex[v].y;delay_fps(60)){
+			xx = xx + 50;
+			yy = yy + 50;
+			putImg(xx, yy);
+		}
+		putImg((vex[v].x+xx)/6, (vex[v].y+yy)/6);
+		
+		putImg((vex[v].x + xx)* 2 / 3, (vex[v].y + yy)* 2 / 3);
+		
+		putImg(vex[v].x, vex[v].y);
+		outtextxy(vex[k].x - 5, vex[k].y + 45, vex[k].Vex);
+		xx = vex[k].x;
+		yy = vex[k].y;
+		k = P[k][w];
+	}
+	//printf("%s %d %d\n", vex[w].Vex, vex[w].x, vex[w].y);//终点名字，x坐标，y坐标
+	putImg((vex[w].x), (vex[w].y));
 }
 
 
-void Mouse1(MGraph *G) {//点击功能模块，返回其对应序号
+void Mouse1(MGraph *G) {//二级菜单，点击功能模块
 	mouse_msg msg = { 0 };
 	for (; is_run(); delay_fps(60))
 	{
+		cleardevice();
+		putBackgroundimgsky(3);
+		//putMenu();
+		setfont(15, 7, "华文琥珀");
+		xyprintf(0, 0, "%d %d",
+			msg.x, msg.y, msg.wheel);
 		//获取鼠标消息，这个函数会等待，等待到有消息为止
 		//类似地，有和kbhit功能相近的函数MouseHit，用于检测有没有鼠标消息
 		int X, Y;
@@ -223,12 +282,165 @@ void Mouse1(MGraph *G) {//点击功能模块，返回其对应序号
 		(int)msg.is_wheel(),
 		msg.wheel);*/
 		mousepos(&X, &Y);
-		if (X >= 280 && X <= 415 && Y >= 160 && Y <= 195 && msg.is_left()) {
-			pageCreate();
-			Mouse2(G);
+		if (X >= 655 && X <= 805 && Y >= 190 && Y <= 225 ) {
+			putBackgroundimgsky(6);
+			if (msg.is_left()) {
+				pageCreate(1);
+				Mouse2(G,1);
+			}
 		}
-		else if (X >= 280 && X <= 415 && Y >= 200 && Y <= 235 && msg.is_left()) {
-
+		else if (X >= 655 && X <= 805 && Y >= 240 && Y <= 275 ) {
+			putBackgroundimgsky(7);
+			if (msg.is_left()) {
+				pageCreate(2);
+				Mouse2(G,2);
+			}
+		}
+		else if (X >= 655 && X <= 805 && Y >= 295 && Y <= 325 ) {
+			putBackgroundimgsky(8);
+			if (msg.is_left()) {
+				pageCreate(3);
+				Mouse2(G,3);
+			}
+		}
+		/*else if (X >= 280 && X <= 445 && Y >= 280 && Y <= 315 && msg.is_left()) {
+		return 4;
+		}
+		else if (X >= 280 && X <= 460 && Y >= 320 && Y <= 355 && msg.is_left()) {
+		return 5;
+		}
+		else if (X >= 280 && X <= 415 && Y >= 360 && Y <= 395 && msg.is_left()) {
+		return 6;
+		}*/
+		else if (X >= 655 && X <= 805 && Y >= 344 && Y <= 375 ) {
+			putBackgroundimgsky(9);
+			if (msg.is_left()) {
+				Mouse0(G);
+			}
+		}
+	}
+}
+void Mouse2(MGraph *G,int i) {//三级菜单，点击功能模块
+	mouse_msg msg = { 0 };
+	for (; is_run(); delay_fps(60))
+	{
+		//获取鼠标消息，这个函数会等待，等待到有消息为止
+		//类似地，有和kbhit功能相近的函数MouseHit，用于检测有没有鼠标消息
+		cleardevice();
+		putBgMap(i);
+		putFunMenu(0);
+		setfont(15, 7, "华文琥珀");
+		xyprintf(0, 0, "%d %d",
+			msg.x, msg.y, msg.wheel);
+		int X, Y;
+		while (mousemsg())
+		{
+			msg = getmouse();
+		}
+		//格式化输出为字符串，用于后面输出
+		//msg和flag常数请参考文档或者mouse_msg_e, mouse_flag_e的声明
+		/*xyprintf(0, 0, "x = %10d  y = %10d",
+		msg.x, msg.y, msg.wheel);
+		xyprintf(0, 20, "move  = %d down  = %d up    = %d",
+		(int)msg.is_move(),
+		(int)msg.is_down(),
+		(int)msg.is_up());
+		xyprintf(0, 40, "left  = %d mid   = %d right = %d",
+		(int)msg.is_left(),
+		(int)msg.is_mid(),
+		(int)msg.is_right());
+		xyprintf(0, 60, "wheel = %d  wheel rotate = %d",
+		(int)msg.is_wheel(),
+		msg.wheel);*/
+		mousepos(&X, &Y);
+		if (X >= 815 && X <= 1000 && Y >= 70 && Y <= 85) {
+			cleardevice();
+			putBgMap(i);
+			putFunMenu(1);
+			if(msg.is_left()) 
+				CreateMGraph(G);
+		}
+		else if (X >= 815 && X <= 1000 && Y >= 100 && Y <= 115) {
+			cleardevice();
+			putBgMap(i);
+			putFunMenu(2);
+			if (msg.is_left())
+				CreateRoad(G);
+		}
+		else if (X >= 815 && X <= 1000 && Y >= 130 && Y <= 145) {
+			cleardevice();
+			putBgMap(i);
+			putFunMenu(3);
+			DFSTraverse(*G);
+		}
+		else if (X >= 815 && X <= 1000 && Y >= 160 && Y <= 175) {
+			cleardevice();
+			putBgMap(i);
+			putFunMenu(4);
+			DFSTraverse(*G);
+			MiniTree_Prim(*G);
+		}
+		else if (X >= 815 && X <= 1000 && Y >= 190 && Y <= 205 && msg.is_left()) {
+			cleardevice();
+			putBgMap(i);
+			int P[30][30]; int D[30][30];
+			ShortPath_Floyd(*G,P,D);
+		}
+		/*else if (X >= 280 && X <= 415 && Y >= 360 && Y <= 395 && msg.is_left()) {
+		return 6;
+		}*/
+		else if (X >= 815 && X <= 1000 && Y >= 230 && Y <= 245) {
+			cleardevice();
+			putBgMap(i);
+			putFunMenu(5);
+			if (msg.is_left()) {
+				putBackgroundimgsky(1);
+				putMenu();
+				Mouse1(G);
+			}
+		}
+	}
+}
+void Mouse0(MGraph *G) {//一级菜单
+	mouse_msg msg = { 0 };
+	for (; is_run(); delay_fps(60))
+	{
+		cleardevice();
+		putBackgroundimgsky(1);
+		//putMenu();
+		setfont(15, 7, "华文琥珀");
+		xyprintf(0, 0, "%d %d",
+			msg.x, msg.y, msg.wheel);
+		//获取鼠标消息，这个函数会等待，等待到有消息为止
+		//类似地，有和kbhit功能相近的函数MouseHit，用于检测有没有鼠标消息
+		int X, Y;
+		while (mousemsg())
+		{
+			msg = getmouse();
+		}
+		//格式化输出为字符串，用于后面输出
+		//msg和flag常数请参考文档或者mouse_msg_e, mouse_flag_e的声明
+		/*xyprintf(0, 0, "x = %10d  y = %10d",
+		msg.x, msg.y, msg.wheel);
+		xyprintf(0, 20, "move  = %d down  = %d up    = %d",
+		(int)msg.is_move(),
+		(int)msg.is_down(),
+		(int)msg.is_up());
+		xyprintf(0, 40, "left  = %d mid   = %d right = %d",
+		(int)msg.is_left(),
+		(int)msg.is_mid(),
+		(int)msg.is_right());
+		xyprintf(0, 60, "wheel = %d  wheel rotate = %d",
+		(int)msg.is_wheel(),
+		msg.wheel);*/
+		mousepos(&X, &Y);
+		if (X >= 390 && X <= 540 && Y >= 190 && Y <= 230 ) {
+			putBackgroundimgsky(2);
+			if (msg.is_left()) 
+				Mouse1(G);
+		}
+		else if (X >= 390 && X <= 540 && Y >= 276 && Y <= 314 ) {
+			putBackgroundimgsky(4);
 		}
 		/*else if (X >= 280 && X <= 385 && Y >= 240 && Y <= 275 && msg.is_left()) {
 		return 3;
@@ -242,60 +454,10 @@ void Mouse1(MGraph *G) {//点击功能模块，返回其对应序号
 		else if (X >= 280 && X <= 415 && Y >= 360 && Y <= 395 && msg.is_left()) {
 		return 6;
 		}*/
-		else if (X >= 280 && X <= 340 && Y >= 240 && Y <= 275 && msg.is_left()) {
+		else if (X >= 390 && X <= 540 && Y >= 370 && Y <= 405) {
+			putBackgroundimgsky(5);
+			if(msg.is_left())
 			exit(0);
-		}
-	}
-}
-void Mouse2(MGraph *G) {//点击功能模块，返回其对应序号
-	mouse_msg msg = { 0 };
-	for (; is_run(); delay_fps(60))
-	{
-		//获取鼠标消息，这个函数会等待，等待到有消息为止
-		//类似地，有和kbhit功能相近的函数MouseHit，用于检测有没有鼠标消息
-		int X, Y;
-		while (mousemsg())
-		{
-			msg = getmouse();
-		}
-		//格式化输出为字符串，用于后面输出
-		//msg和flag常数请参考文档或者mouse_msg_e, mouse_flag_e的声明
-		/*xyprintf(0, 0, "x = %10d  y = %10d",
-		msg.x, msg.y, msg.wheel);
-		xyprintf(0, 20, "move  = %d down  = %d up    = %d",
-		(int)msg.is_move(),
-		(int)msg.is_down(),
-		(int)msg.is_up());
-		xyprintf(0, 40, "left  = %d mid   = %d right = %d",
-		(int)msg.is_left(),
-		(int)msg.is_mid(),
-		(int)msg.is_right());
-		xyprintf(0, 60, "wheel = %d  wheel rotate = %d",
-		(int)msg.is_wheel(),
-		msg.wheel);*/
-		mousepos(&X, &Y);
-		if (X >= 815 && X <= 1000 && Y >= 70 && Y <= 85 && msg.is_left()) {
-			CreateMGraph(G);			
-		}
-		else if (X >= 815 && X <= 1000 && Y >= 100 && Y <= 115 && msg.is_left()) {
-			CreateRoad(G);
-		}
-		else if (X >= 815 && X <= 1000 && Y >= 130 && Y <= 145 && msg.is_left()) {
-			DFSTraverse(*G);
-		}
-		/*else if (X >= 280 && X <= 445 && Y >= 280 && Y <= 315 && msg.is_left()) {
-		return 4;
-		}
-		else if (X >= 280 && X <= 460 && Y >= 320 && Y <= 355 && msg.is_left()) {
-		return 5;
-		}
-		else if (X >= 280 && X <= 415 && Y >= 360 && Y <= 395 && msg.is_left()) {
-		return 6;
-		}*/
-		else if (X >= 815 && X <= 1000 && Y >= 230 && Y <= 245 && msg.is_left()) {
-			putBackgroundimgsky();
-			putMenu();
-			Mouse1(G);
 		}
 	}
 }
