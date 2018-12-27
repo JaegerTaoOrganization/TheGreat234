a07fe8a0b25acecd5dbe77a330d47821b545caf7
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-27 08:46:16 +0800

    Update FunMenu.cpp

--- FunMenu.cpp
+++ FunMenu.cpp
@@ -1,20 +1,57 @@
 #include"SATMS.h"
-void putBackgroundimgsky() {
+void putBackgroundimgsky(int i) {
 	//输出菜单背景图片
 	PIMAGE imgsky = newimage(100, 50);//初始化
-	getimage(imgsky, "D:\\Picture\\view.jpg", 0, 0);//获取图片地址
-	putimage(0, 0, imgsky);//放置图片
+	switch (i)
+	{
+	case 1:getimage(imgsky, "D:\\Picture\\Menu.png", 0, 0);//获取图片地址
+		break;
+	case 2:getimage(imgsky, "D:\\Picture\\Menu2.png", 0, 0);//获取图片地址
+		break;
+	case 3:getimage(imgsky, "D:\\Picture\\Menu3.png", 0, 0);//获取图片地址
+		break;
+	case 4:getimage(imgsky, "D:\\Picture\\Menu4.png", 0, 0);//获取图片地址
+		break;
+	case 5:getimage(imgsky, "D:\\Picture\\Menu5.png", 0, 0);//获取图片地址
+		break;
+	case 6:getimage(imgsky, "D:\\Picture\\Menu6.png", 0, 0);//获取图片地址
+		break;
+	case 7:getimage(imgsky, "D:\\Picture\\Menu7.png", 0, 0);//获取图片地址
+		break;
+	case 8:getimage(imgsky, "D:\\Picture\\Menu8.png", 0, 0);//获取图片地址
+		break;
+	case 9:getimage(imgsky, "D:\\Picture\\Menu9.png", 0, 0);//获取图片地址
+		break;
+	default:
+		break;
+	}
+	putimage(0, 0, 1000,600,imgsky,0,160,1890,1061);//放置图片
 	delimage(imgsky);//释放图片指针
 	/*setbkcolor(WHITE);*/
 }
-void putBgMap() {
+void putBgMap(int i) {
 	//输出地图背景
 	setbkcolor(WHITE);
 	PIMAGE imgBg = newimage(100, 50);//初始化
-	getimage(imgBg, "D:\\Picture\\岛屿.png", 0, 0);//获取图片地址
-	putimage(0, 0,800,605, imgBg,0,0,852,746);//放置图片
+	switch (i)
+	{
+	case 1: getimage(imgBg, "D:\\Picture\\岛屿.png", 0, 0);//获取图片地址
+		putimage(0, 0, 800, 605, imgBg, 0, 0, 852, 746);//放置图片
+		break;
+	case 2: getimage(imgBg, "D:\\Picture\\map.png", 0, 0);//获取图片地址
+		putimage(0, 0, 800, 605, imgBg, 0, 0, 479, 426);//放置图片
+		break;
+	case 3: getimage(imgBg, "D:\\Picture\\map2.jpg", 0, 0);//获取图片地址
+		putimage(0, 0, 800, 605, imgBg, 0, 0, 858, 504);//放置图片
+		break;
+	default:
+		break;
+	}
 	delimage(imgBg);//释放图片指针
-	putFunMenu();
+	PIMAGE walk = newimage(100, 50);//初始化
+	getimage(walk, "D:\\Picture\\walk.gif", 0, 0);//获取图片地址
+	putimage(800, 300, 200, 300, walk, 50, 0, 250, 350);//放置图片
+	delimage(walk);//释放图片指针
 }
 void putMenu() {
 	//菜单的输出
@@ -40,18 +77,76 @@ void putMenu() {
 	//outtextxy(280, 360, "输出道路修建规划图");//x:280-415,y:360-395
 	outtextxy(280, 240, "退出程序");//x:280-340,y:400-435
 }
-void putFunMenu() {
+void putFunMenu(int i) {
 	setcolor(BLACK);
 	setfont(25, 12, "华文琥珀");
 	outtextxy(820, 10, "景区管理界面");
 	setfont(15, 7, "黑体");
 	outtextxy(815, 40, "可点击的功能选项：");
-	outtextxy(820, 70,  "创建景区景点");
-	outtextxy(820, 100, "创建道路");
-	outtextxy(820, 130, "输出导游路线图");
-	outtextxy(820, 230, "返回");
+	switch (i)
+	{
+	case 0:{
+		outtextxy(820, 70, "创建景区景点");
+		outtextxy(820, 100, "创建道路");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		outtextxy(820, 190, "两点最短路线");
+		outtextxy(820, 230, "返回"); 
+		break; }
+	case 1: {
+		setcolor(RED);
+		setfont(15, 7, "华文琥珀");
+		outtextxy(820, 70, "创建景区景点");
+		setcolor(BLACK);
+		setfont(15, 7, "黑体");
+		outtextxy(820, 100, "创建道路");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		outtextxy(820, 230, "返回");
+		break; }
+	case 2:{
+		outtextxy(820, 70, "创建景区景点");
+		setcolor(RED);
+		setfont(15, 7, "华文琥珀");
+		outtextxy(820, 100, "创建道路");
+		setcolor(BLACK);
+		setfont(15, 7, "黑体");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		outtextxy(820, 230, "返回"); break; }
+	case 3: {
+		outtextxy(820, 70, "创建景区景点");
+		outtextxy(820, 100, "创建道路");
+		setcolor(RED);
+		setfont(15, 7, "华文琥珀");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		setcolor(BLACK);
+		setfont(15, 7, "黑体");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		outtextxy(820, 230, "返回"); break; }
+	case 4: {
+		outtextxy(820, 70, "创建景区景点");
+		outtextxy(820, 100, "创建道路");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		setcolor(RED);
+		setfont(15, 7, "华文琥珀");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		setcolor(BLACK);
+		setfont(15, 7, "黑体");
+		outtextxy(820, 230, "返回"); break; }
+	case 5: {
+		outtextxy(820, 70, "创建景区景点");
+		outtextxy(820, 100, "创建道路");
+		outtextxy(820, 130, "鼠标移到这输出导游路线图");
+		outtextxy(820, 160, "鼠标移到这输出道路规划");
+		setcolor(RED);
+		setfont(15, 7, "华文琥珀");
+		outtextxy(820, 230, "返回"); break; }
+	default:
+		break;
+	}
+	
 }
-
 int input() {
 	//从键盘读入数字字符，返回该数字int型
 	setcolor(BLACK);
@@ -89,7 +184,6 @@ int inputInfoY() {
 }
 
 void putline(int a, int b, int c, int d) {
-	setcolor(GREEN);
 	setlinestyle(DASHED_LINE, NULL, 6);
 	line(a, b, c, d);
 }
