d360223bc3578116cd39a8974ceaef68e48f9f07
JaegerTao <46061258+JaegerTao@users.noreply.github.com>
2018-12-27 08:45:24 +0800

    Update SATMS.h

--- SATMS.h
+++ SATMS.h
@@ -6,11 +6,11 @@
 #include<iostream>
 using namespace std;
 //图片输出函数
-void putBackgroundimgsky();//输出菜单背景图片
-void putBgMap();//输出地图背景
+void putBackgroundimgsky(int i);//输出菜单背景图片
+void putBgMap(int i);//输出地图背景
 void putline(int a, int b, int c, int d);//画出点(a,b)到(c,d)的道路
 void putMenu();//输出主菜单
-void putFunMenu();//输出功能菜单
+void putFunMenu(int i);//输出功能菜单
 
 int input();//从键盘读入一个数字字符，返回该数字int型
 int inputInfoY();//输入Y坐标值，并返回该值
